d2cc6d31ea246496c39ba54ddcc70b118c9965e3
Patrick Collins <patrickmc77@hotmail.com>
2024-05-27 21:02:37 -0500

    Added Investing section

---
+++ Investing/Scripts/InvestFnc.js
@@ -0,0 +1,14 @@
+﻿
+//*******************************************************
+//Square Root of (22.5 * TTM Earnings per Share * MRQ Book Value per Share)
+function CalcBenGrahamNum(ttmEps,mrqBv,constVal)
+{
+	if(!constVal){constVal=22.5;}
+	return Math.pow(ttmEps*mrqBv*constVal,.5)
+}
+//*******************************************************
+function CalcPercentOffGrahamNum(price,grahamNum)
+{
+	return 100*price/grahamNum-100;
+}
+//*******************************************************
